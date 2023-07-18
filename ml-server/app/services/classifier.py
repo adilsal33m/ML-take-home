462bbc3591142ff691513f854db72dddab9f294f
Adil Saleem <adilsaleem.92@gmail.com>
2023-07-19 01:09:18 +0500

    Bug fixed: preprocessor_config not found

--- ml-server/app/services/classifier.py
+++ ml-server/app/services/classifier.py
@@ -1,7 +1,7 @@
 from typing import Any, List, Union
 from pathlib import Path
 from PIL import Image
-from transformers import pipeline
+from transformers import pipeline,AutoImageProcessor,AutoModelForImageClassification
 
 from app.models.schemas.classifier import ClassifierResponseSchema
 
@@ -14,7 +14,9 @@ MODEL_NAME = "./mlmodels/vit-potatoes-plant-health-status/"
 class ImageClassifier:
     def __init__(self):
         self.model_name = MODEL_NAME if Path(MODEL_NAME).exists() else MODEL_BASE
-        self.classifier = pipeline("image-classification", model=self.model_name)
+        self.classifier = pipeline("image-classification", 
+                                   model=AutoModelForImageClassification.from_pretrained(self.model_name),
+                                   feature_extractor=AutoImageProcessor.from_pretrained(self.model_name))
 
         if not Path(MODEL_NAME).exists():
             self.classifier.save_pretrained(MODEL_NAME)

c7570134dd57861893530eb8664fdfac388cbc0b
Adil Saleem <adilsaleem.92@gmail.com>
2023-07-26 22:13:44 +0500

    Test updated

--- ml-server/tests/test_base.py
+++ ml-server/tests/test_base.py
@@ -1,3 +1,5 @@
+import os
+
 def test_base(app_client):
     response = app_client.get("/")
     assert response.status_code == 200
@@ -35,3 +37,24 @@ def test_classify_late_blight(app_client):
     assert response.json()["message"] == "Successful classification"
     assert response.json()["data"][0][0]["label"] == "Late_Blight"
     assert response.json()["data"][0][0]["score"] > 0.9
+
+def test_bad_validation_images(app_client):
+    path = r"E:/dev/ML-take-home/ml-client/public/images/PLD_3_Classes_256/test"
+
+    dirty = []
+
+    for root, dirs, files in os.walk(path, topdown=False):
+        for name in files:
+            if ".jpg" in name:
+                response = app_client.post(
+                    "/classify",
+                    files={"file": open(os.path.join(root, name), "rb")},
+                )
+
+                if response.json()["data"][0][0]["label"] not in name:
+                    dirty.append(name)
+
+        print(dirty)
+
+        assert len(dirty) == 0
+                
